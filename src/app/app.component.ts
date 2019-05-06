b928cbdf3c3416f5f1ac7f809e0bb554f62a36fd
khokan1122 <khokanuzzamankhokan@gmail.com>
2019-05-07 04:07:47 +0600

    initial working for todo-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { fakeAsync } from '@angular/core/testing';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  todos:any[]=[
+    {
+      lebel:'Bring Milk',
+      done:false,
+      priority:3
+    
+    },
+    {
+      lebel:'Pay Mobile Bill',
+      done:false,
+      priority:1
+    
+    },
+    {
+      lebel:'clean house',
+      done:true,
+      priority:3
+    
+    },
+    {
+      lebel:'fix the bulb',
+      done:false,
+      priority:3
+    
+    }
+  ];
+
+  addTodo(newtodoValue)
+  {
+    var newTodo=
+    {
+      lebel:newtodoValue,
+      done: false,
+      priority:1
+    };
+    this.todos.push(newTodo);
+  }
+
+  deleteTodo(todo)
+  {
+    this.todos= this.todos.filter(t=>t.lebel!==todo.lebel);
+  }
 }
+
+
+
